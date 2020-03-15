d96eadd49078cb6d7526d0376c52fb597f38caac
codekid <ckrackerx15@gmail.com>
2020-03-15 12:45:01 +0530

    expose result and see

--- check/int.go
+++ check/int.go
@@ -2,32 +2,32 @@ package check
 
 type intchecker struct {
 	target int
-	result bool
+	Result bool
 }
 
 func Int(target int) intchecker {
 	return intchecker{
 		target: target,
-		result: true,
+		Result: true,
 	}
 }
 
 func (ic intchecker) IsPositive() intchecker {
-	ic.result = ic.result && ic.target > 0
+	ic.Result = ic.Result && ic.target > 0
 	return ic
 }
 
 func (ic intchecker) IsNegative() intchecker {
-	ic.result = ic.result && ic.target < 0
+	ic.Result = ic.Result && ic.target < 0
 	return ic
 }
 
 func (ic intchecker) IsZero() intchecker {
-	ic.result = ic.result && ic.target == 0
+	ic.Result = ic.Result && ic.target == 0
 	return ic
 }
 
 func (ic intchecker) IsInBetween(min, max int) intchecker {
-	ic.result = ic.result && ic.target >= min && ic.target <= max
+	ic.Result = ic.Result && ic.target >= min && ic.target <= max
 	return ic
 }
