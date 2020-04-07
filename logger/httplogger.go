69683a272395e0ec906ece1015ea9d0c4d64450f
codekid <ckrackerx15@gmail.com>
2020-04-07 09:20:35 +0530

    keep it simple for now

--- logger/httplogger.go
+++ logger/httplogger.go
@@ -18,33 +18,39 @@ type BlockLogger struct {
 }
 
 type blockLoggerConfig struct {
-	Level string `json:"level"`
+	Level  string `json:"level"`
+	Format string `json:"format"`
 }
 
 // OnAttach implementation of rubik block
 func (bl BlockLogger) OnAttach(app *rubik.App) error {
 	var config blockLoggerConfig
+
 	err := app.Decode("logger", &config)
 	if err != nil {
 		return err
 	}
+
 	bl.config = config
+	if config.Format == "" {
+		bl.config.Format = "[%s] Method:%s - [%s] [%d] - %fms"
+	}
 	rubik.BeforeRequest(bl.beforeHook)
 	rubik.AfterRequest(bl.afterHook)
+
 	return nil
 }
 
-func (bl BlockLogger) beforeHook(rc rubik.RequestContext) {
+func (bl BlockLogger) beforeHook(rc *rubik.RequestContext) {
 	rc.Ctx["from"] = time.Now()
 }
 
-func (bl BlockLogger) afterHook(rc rubik.RequestContext) {
+func (bl BlockLogger) afterHook(rc *rubik.RequestContext) {
 	fromTime, _ := rc.Ctx["from"].(time.Time)
-	diff := time.Now().Sub(fromTime)
-	responseTime := diff.Seconds()
+	responseTime := time.Since(fromTime).Seconds() * 100000
 	layout := "Mon, 2 Jan 2006 15:04:05 MST"
 	logTime := time.Now().Format(layout)
-	logMsg := fmt.Sprintf("[%s] %s - [%s] [%d] - %fms", logTime, rc.Request.Method,
+	logMsg := fmt.Sprintf(bl.config.Format, logTime, rc.Request.Method,
 		rc.Request.URL.Path, rc.Status, responseTime)
 	fmt.Println(logMsg)
 }
